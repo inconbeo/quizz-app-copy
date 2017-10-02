115bd953f0a44efebc73973fe43eadb867b16f12
Andy Gaines <andyagaines@gmail.com>
2017-10-02 10:51:53 -0500

    added basic javascript and html

---
+++ quiz.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const quizQuestions = [{question: x, corAnswer: y, answers: {a: adfa, b: adfad, c: daf, d: adf}}]
+
+const STORE = {
+  view: 'intro',
+  questions: [{}, {}, {}, {}, {}],
+  choice: null,
+  currentQuestion: null,
+  correctResponses: null,
+}
